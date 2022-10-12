1d60a7ecfd3cda4f84bf497de52c6ba8f8bfec1c
Sasano63 <sasano@hashicorp.com>
2022-10-12 15:39:21 +0200

    added subnet

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "primary_vault_public_endpoint_url" {
   description = "The public IP address of the cluster."
 }
 
+output "primary_vault_private_endpoint_url" {
+  value       = hcp_vault_cluster.vault-demo-cluster.vault_private_endpoint_url
+  description = "The private IP address of the cluster."
+}
+
 output "namespace" {
   value       = hcp_vault_cluster.vault-demo-cluster.namespace
   description = "The default namespace of the cluster."
